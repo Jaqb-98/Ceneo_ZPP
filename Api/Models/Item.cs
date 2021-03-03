f2b7e067f3deacb69c5481f61b044a065302c9bb
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-03 11:06:03 +0100

    Item basic model

---
+++ Api/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Item
+    {
+        [BsonId]
+        public int PID { get; set; }
+        //[BsonElement("Name")]
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
