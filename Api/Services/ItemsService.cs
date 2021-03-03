c37ff1c73d8386d1b715975294fb05858d785858
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-03 11:07:31 +0100

    Services

---
+++ Api/Services/ItemsService.cs
@@ -0,0 +1,56 @@
+﻿using Api.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Api.Services
+{
+    
+    public class ItemsService
+    {
+        private readonly IMongoCollection<Item> _items;
+
+        public ItemsService(IItemsDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _items = database.GetCollection<Item>(settings.ItemsCollectionName);
+        }
+
+        public List<Item> Get() =>
+            _items.Find(item => true).ToList();
+
+        public Item Get(int id) =>
+            _items.Find<Item>(item => item.PID == id).FirstOrDefault();
+
+        public Item Create(Item item)
+        {
+            _items.InsertOne(item);
+            return item;
+        }
+
+        public List<Item> CreateMany(List<Item> items)
+        {
+            _items.InsertManyAsync(items);
+            return items;
+        }
+
+
+        public void Update(int id, Item ItemIn) =>
+            _items.ReplaceOne(item => item.PID == id, ItemIn);
+
+        public void Remove(Item ItemIn) =>
+            _items.DeleteOne(item => item.PID == ItemIn.PID);
+
+        public void Remove(int id) =>
+            _items.DeleteOne(item => item.PID == id);
+
+        public void Upsert(int id, Item ItemIn) =>
+            _items.ReplaceOne(new BsonDocument("_id", id), ItemIn, new ReplaceOptions { IsUpsert = true });
+
+
+
+    }
+}
