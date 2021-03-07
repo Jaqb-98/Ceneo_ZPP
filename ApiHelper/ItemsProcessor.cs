2b0406cd10ee077526beffb7f78f4359d360effa
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-07 10:48:00 +0100

    Operations with items

---
+++ ApiHelper/ItemsProcessor.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Helper
+{
+    public static class ItemsProcessor
+    {
+
+        public static async Task<List<Item>> LoadItems(string searchingPhrase, int page = 1)
+        {
+            string url = $"https://localhost:44343/api/Items/Parse?name={ searchingPhrase }&page={ page }";
+
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Item> items = await response.Content.ReadAsAsync<List<Item>>();
+
+                    return items;
+                }
+                else
+                    throw new Exception(response.ReasonPhrase);
+            }
+
+
+
+        }
+
+
+        public static async Task SaveItemsToDB(List<Item> items)
+        {
+            string url = $"https://localhost:44343/api/Items/UpsertItems";
+            var httpContent = new StringContent(items.ToString(),Encoding.UTF8, "application/json");
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PutAsync(url, httpContent))
+            {
+                if (response.IsSuccessStatusCode)
+                    throw new Exception(response.ReasonPhrase);
+            }
+
+
+
+        }
+
+
+
+
+    }
+}
