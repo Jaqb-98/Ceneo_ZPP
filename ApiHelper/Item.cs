357365d85beebc1ab88e4bfdd104ad396eef8820
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-07 10:46:33 +0100

    Item Model

---
+++ ApiHelper/Item.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Helper
+{
+    public class Item
+    {
+        [BsonId]
+        public int PID { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
