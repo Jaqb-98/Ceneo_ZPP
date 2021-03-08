d18e0722973a2a394fdc9b7df923c9142658d416
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-08 10:35:56 +0100

    WebsiteParser interface

---
+++ Api/Interfaces/IWebsiteParser.cs
@@ -0,0 +1,13 @@
+﻿using Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Interfaces
+{
+    public interface IWebsiteParser
+    {
+        List<Item> GetItems(string searchedItem, int page);
+    }
+}
