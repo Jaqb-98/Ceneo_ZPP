02d6ab79cf8d404232f49a5309a20e6d28a61115
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-08 10:35:11 +0100

    ItemsService interface with all crud methods

---
+++ Api/Interfaces/IItemsService.cs
@@ -0,0 +1,31 @@
+﻿using Helper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Interfaces
+{
+    public interface IItemsService
+    {
+        List<Item> Get();
+
+        Item Get(int id);
+
+        Item Create(Item item);
+
+        List<Item> CreateMany(List<Item> items);
+
+        void Update(int id, Item ItemIn);
+
+        void Remove(Item ItemIn);
+
+        void Remove(int id);
+
+        void Upsert(int id, Item ItemIn);
+
+        void Upsert(List<Item> items);
+
+    }
+}
