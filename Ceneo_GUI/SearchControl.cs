43cc06f835282fa37a7c5b4b22090a8cf6aef9e6
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-14 14:36:56 +0100

    deleted

--- Ceneo_GUI/SearchControl.cs
+++ Ceneo_GUI/SearchControl.cs
@@ -20,51 +20,51 @@ namespace test
             InitializeComponent();
         }
 
-        public void Funcja()
-        {
+        //public void Funcja()
+        //{
             
-            for (int i = 0; i < ileOkienek; i++)
-            {
+        //    for (int i = 0; i < ileOkienek; i++)
+        //    {
 
-                ItemControl searchControl2 = new ItemControl();
-                searchControl2.Location = new Point(6, 3 + (i * 100));
+        //        ItemControl searchControl2 = new ItemControl();
+        //        searchControl2.Location = new Point(6, 3 + (i * 100));
 
-                searchControl2.myIndex = Controls.Count;
-                Controls.Add(searchControl2);
+        //        searchControl2.myIndex = Controls.Count;
+        //        Controls.Add(searchControl2);
                 
-            }
-        }
+        //    }
+        //}
 
         private void SearchControl_Load(object sender, EventArgs e)
         {
 
         }
 
-        public void test3(int x) {
-            Controls.Clear();
-            int afterBigOne = 0;
-            for (int i = 0; i < ileOkienek; i++) {
+        //public void test3(int x) {
+        //    Controls.Clear();
+        //    int afterBigOne = 0;
+        //    for (int i = 0; i < ileOkienek; i++) {
 
                 
 
-                if (i == x) {
-                    CompareControl2 compareControl2 = new CompareControl2();
-                    compareControl2.Location = new Point(6, 3 + (i * 100));
-                    compareControl2.BackColor = Color.Red;
-                    compareControl2.myIndex = Controls.Count;
-                    Controls.Add(compareControl2);
-                    afterBigOne = 100;
-                }
-                else {
+        //        if (i == x) {
+        //            CompareControl2 compareControl2 = new CompareControl2();
+        //            compareControl2.Location = new Point(6, 3 + (i * 100));
+        //            compareControl2.BackColor = Color.Red;
+        //            compareControl2.myIndex = Controls.Count;
+        //            Controls.Add(compareControl2);
+        //            afterBigOne = 100;
+        //        }
+        //        else {
 
-                    ItemControl searchControl2 = new ItemControl();
-                    searchControl2.Location = new Point(6, 3 + (i * 100) + afterBigOne);
+        //            ItemControl searchControl2 = new ItemControl();
+        //            searchControl2.Location = new Point(6, 3 + (i * 100) + afterBigOne);
 
-                    searchControl2.myIndex = Controls.Count;
-                    Controls.Add(searchControl2);
-                }
-            }
-        }
+        //            searchControl2.myIndex = Controls.Count;
+        //            Controls.Add(searchControl2);
+        //        }
+        //    }
+        //}
 
 
     }
