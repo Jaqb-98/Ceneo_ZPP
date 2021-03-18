b648355f7e84d3b430ed360a9e418eb95437d0e7
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-18 19:32:36 +0100

    Deleted unnecessary controls

--- Ceneo.GUI/MainForm.Designer.cs
+++ Ceneo.GUI/MainForm.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.txtbSearchBar = new System.Windows.Forms.TextBox();
-            this.btnLogIn = new System.Windows.Forms.Button();
-            this.TextNameLogin = new System.Windows.Forms.TextBox();
-            this.btnSearch = new System.Windows.Forms.Button();
             this.btnHome = new System.Windows.Forms.Button();
             this.flItemListPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.btnNextPage = new System.Windows.Forms.Button();
             this.btnPreviousPage = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtbSearchBar = new System.Windows.Forms.TextBox();
+            this.searchPanel = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
+            this.searchPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -65,55 +65,6 @@
             this.textBox1.Size = new System.Drawing.Size(171, 65);
             this.textBox1.TabIndex = 8;
             this.textBox1.Text = "Kategorie:";
-            // 
-            // txtbSearchBar
-            // 
-            this.txtbSearchBar.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtbSearchBar.Location = new System.Drawing.Point(257, 40);
-            this.txtbSearchBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtbSearchBar.Multiline = true;
-            this.txtbSearchBar.Name = "txtbSearchBar";
-            this.txtbSearchBar.Size = new System.Drawing.Size(964, 47);
-            this.txtbSearchBar.TabIndex = 3;
-            // 
-            // btnLogIn
-            // 
-            this.btnLogIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(0)))));
-            this.btnLogIn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnLogIn.Location = new System.Drawing.Point(1256, 40);
-            this.btnLogIn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnLogIn.Name = "btnLogIn";
-            this.btnLogIn.Size = new System.Drawing.Size(133, 48);
-            this.btnLogIn.TabIndex = 5;
-            this.btnLogIn.Text = "Zaloguj się!";
-            this.btnLogIn.UseVisualStyleBackColor = false;
-            this.btnLogIn.Click += new System.EventHandler(this.LoginButton_Click);
-            // 
-            // TextNameLogin
-            // 
-            this.TextNameLogin.Location = new System.Drawing.Point(1256, 106);
-            this.TextNameLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.TextNameLogin.Multiline = true;
-            this.TextNameLogin.Name = "TextNameLogin";
-            this.TextNameLogin.Size = new System.Drawing.Size(132, 46);
-            this.TextNameLogin.TabIndex = 6;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(0)))));
-            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Default;
-            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnSearch.ForeColor = System.Drawing.Color.White;
-            this.btnSearch.Location = new System.Drawing.Point(1076, 40);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(147, 49);
-            this.btnSearch.TabIndex = 7;
-            this.btnSearch.Text = "Szukaj!";
-            this.btnSearch.UseVisualStyleBackColor = false;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_click);
             // 
             // btnHome
             // 
@@ -145,9 +96,10 @@
             // 
             // btnNextPage
             // 
+            this.btnNextPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnNextPage.BackColor = System.Drawing.Color.Peru;
             this.btnNextPage.ForeColor = System.Drawing.Color.Black;
-            this.btnNextPage.Location = new System.Drawing.Point(1327, 819);
+            this.btnNextPage.Location = new System.Drawing.Point(1354, 722);
             this.btnNextPage.Name = "btnNextPage";
             this.btnNextPage.Size = new System.Drawing.Size(60, 55);
             this.btnNextPage.TabIndex = 11;
@@ -157,9 +109,10 @@
             // 
             // btnPreviousPage
             // 
+            this.btnPreviousPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnPreviousPage.BackColor = System.Drawing.Color.Peru;
             this.btnPreviousPage.ForeColor = System.Drawing.Color.Black;
-            this.btnPreviousPage.Location = new System.Drawing.Point(1229, 819);
+            this.btnPreviousPage.Location = new System.Drawing.Point(1255, 722);
             this.btnPreviousPage.Name = "btnPreviousPage";
             this.btnPreviousPage.Size = new System.Drawing.Size(87, 55);
             this.btnPreviousPage.TabIndex = 12;
@@ -167,21 +120,53 @@
             this.btnPreviousPage.UseVisualStyleBackColor = false;
             this.btnPreviousPage.Click += new System.EventHandler(this.btnPreviousPage_Click);
             // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(0)))));
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(1060, 27);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(147, 47);
+            this.btnSearch.TabIndex = 7;
+            this.btnSearch.Text = "Szukaj!";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_click);
+            // 
+            // txtbSearchBar
+            // 
+            this.txtbSearchBar.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbSearchBar.Location = new System.Drawing.Point(257, 40);
+            this.txtbSearchBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtbSearchBar.Multiline = true;
+            this.txtbSearchBar.Name = "txtbSearchBar";
+            this.txtbSearchBar.Size = new System.Drawing.Size(809, 47);
+            this.txtbSearchBar.TabIndex = 3;
+            // 
+            // searchPanel
+            // 
+            this.searchPanel.Controls.Add(this.btnSearch);
+            this.searchPanel.Location = new System.Drawing.Point(16, 13);
+            this.searchPanel.Name = "searchPanel";
+            this.searchPanel.Size = new System.Drawing.Size(1398, 87);
+            this.searchPanel.TabIndex = 13;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1408, 895);
+            this.ClientSize = new System.Drawing.Size(1426, 895);
+            this.Controls.Add(this.txtbSearchBar);
             this.Controls.Add(this.btnPreviousPage);
             this.Controls.Add(this.btnNextPage);
             this.Controls.Add(this.flItemListPanel);
             this.Controls.Add(this.btnHome);
-            this.Controls.Add(this.btnSearch);
-            this.Controls.Add(this.TextNameLogin);
-            this.Controls.Add(this.btnLogIn);
-            this.Controls.Add(this.txtbSearchBar);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.searchPanel);
             this.ForeColor = System.Drawing.Color.White;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
@@ -189,6 +174,7 @@
             this.Text = "Korkiet_aplikacja";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.searchPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,16 +183,15 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox txtbSearchBar;
-        private System.Windows.Forms.Button btnLogIn;
-        private System.Windows.Forms.TextBox TextNameLogin;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnHome;
         private System.Windows.Forms.FlowLayoutPanel flItemListPanel;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button btnNextPage;
         private System.Windows.Forms.Button btnPreviousPage;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtbSearchBar;
+        private System.Windows.Forms.Panel searchPanel;
     }
 }
 
