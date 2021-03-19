615eae37dc9a06cf813810e55270696129ddd893
Jakub Abramczyk <jakub.abramczyk98@gmail.com>
2021-03-19 18:57:19 +0100

    Added layout improvement

--- Ceneo.GUI/MainForm.Designer.cs
+++ Ceneo.GUI/MainForm.Designer.cs
@@ -28,43 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btnHome = new System.Windows.Forms.Button();
-            this.flItemListPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.components = new System.ComponentModel.Container();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.btnNextPage = new System.Windows.Forms.Button();
-            this.btnPreviousPage = new System.Windows.Forms.Button();
-            this.btnSearch = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.txtbSearchBar = new System.Windows.Forms.TextBox();
-            this.searchPanel = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.searchPanel.SuspendLayout();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.spinningCircles1 = new Ceneo.GUI.SpinningCircles();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.flItemListPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnPreviousPage = new System.Windows.Forms.Button();
+            this.btnNextPage = new System.Windows.Forms.Button();
+            this.lblPageNumber = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // backgroundWorker1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(0)))));
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(16, 106);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(211, 769);
-            this.panel1.TabIndex = 2;
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
             // 
-            // textBox1
+            // tableLayoutPanel1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(0)))));
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
-            this.textBox1.Location = new System.Drawing.Point(21, 23);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(171, 65);
-            this.textBox1.TabIndex = 8;
-            this.textBox1.Text = "Kategorie:";
+            this.tableLayoutPanel1.ColumnCount = 6;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.02062F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.92601F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.05337F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.txtbSearchBar, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnHome, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.spinningCircles1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSearch, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flItemListPanel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnPreviousPage, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnNextPage, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPageNumber, 4, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.610791F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.3892F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1426, 958);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtbSearchBar
+            // 
+            this.txtbSearchBar.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtbSearchBar.Location = new System.Drawing.Point(219, 5);
+            this.txtbSearchBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtbSearchBar.Multiline = true;
+            this.txtbSearchBar.Name = "txtbSearchBar";
+            this.txtbSearchBar.Size = new System.Drawing.Size(809, 47);
+            this.txtbSearchBar.TabIndex = 15;
             // 
             // btnHome
             // 
@@ -73,52 +91,24 @@
             this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnHome.ForeColor = System.Drawing.Color.White;
-            this.btnHome.Location = new System.Drawing.Point(16, 18);
+            this.btnHome.Location = new System.Drawing.Point(4, 5);
             this.btnHome.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(211, 69);
-            this.btnHome.TabIndex = 9;
+            this.btnHome.Size = new System.Drawing.Size(205, 65);
+            this.btnHome.TabIndex = 17;
             this.btnHome.Text = "Krokiet";
             this.btnHome.UseVisualStyleBackColor = false;
-            this.btnHome.Click += new System.EventHandler(this.KrokietButton_Click);
             // 
-            // flItemListPanel
+            // spinningCircles1
             // 
-            this.flItemListPanel.AutoScroll = true;
-            this.flItemListPanel.Location = new System.Drawing.Point(257, 106);
-            this.flItemListPanel.Name = "flItemListPanel";
-            this.flItemListPanel.Size = new System.Drawing.Size(966, 769);
-            this.flItemListPanel.TabIndex = 10;
-            // 
-            // backgroundWorker1
-            // 
-            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker_RunWorkerCompleted);
-            // 
-            // btnNextPage
-            // 
-            this.btnNextPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnNextPage.BackColor = System.Drawing.Color.Peru;
-            this.btnNextPage.ForeColor = System.Drawing.Color.Black;
-            this.btnNextPage.Location = new System.Drawing.Point(1354, 722);
-            this.btnNextPage.Name = "btnNextPage";
-            this.btnNextPage.Size = new System.Drawing.Size(60, 55);
-            this.btnNextPage.TabIndex = 11;
-            this.btnNextPage.Text = "Next";
-            this.btnNextPage.UseVisualStyleBackColor = false;
-            this.btnNextPage.Click += new System.EventHandler(this.btnNextPage_Click);
-            // 
-            // btnPreviousPage
-            // 
-            this.btnPreviousPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnPreviousPage.BackColor = System.Drawing.Color.Peru;
-            this.btnPreviousPage.ForeColor = System.Drawing.Color.Black;
-            this.btnPreviousPage.Location = new System.Drawing.Point(1255, 722);
-            this.btnPreviousPage.Name = "btnPreviousPage";
-            this.btnPreviousPage.Size = new System.Drawing.Size(87, 55);
-            this.btnPreviousPage.TabIndex = 12;
-            this.btnPreviousPage.Text = "Previous";
-            this.btnPreviousPage.UseVisualStyleBackColor = false;
-            this.btnPreviousPage.Click += new System.EventHandler(this.btnPreviousPage_Click);
+            this.spinningCircles1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.spinningCircles1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.SetColumnSpan(this.spinningCircles1, 3);
+            this.spinningCircles1.Location = new System.Drawing.Point(1291, 3);
+            this.spinningCircles1.Name = "spinningCircles1";
+            this.spinningCircles1.Size = new System.Drawing.Size(110, 69);
+            this.spinningCircles1.TabIndex = 18;
+            this.spinningCircles1.Visible = false;
             // 
             // btnSearch
             // 
@@ -127,71 +117,93 @@
             this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnSearch.ForeColor = System.Drawing.Color.White;
-            this.btnSearch.Location = new System.Drawing.Point(1060, 27);
+            this.btnSearch.Location = new System.Drawing.Point(1054, 5);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(147, 47);
-            this.btnSearch.TabIndex = 7;
+            this.btnSearch.TabIndex = 19;
             this.btnSearch.Text = "Szukaj!";
             this.btnSearch.UseVisualStyleBackColor = false;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_click);
             // 
-            // txtbSearchBar
+            // flItemListPanel
             // 
-            this.txtbSearchBar.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtbSearchBar.Location = new System.Drawing.Point(257, 40);
-            this.txtbSearchBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtbSearchBar.Multiline = true;
-            this.txtbSearchBar.Name = "txtbSearchBar";
-            this.txtbSearchBar.Size = new System.Drawing.Size(809, 47);
-            this.txtbSearchBar.TabIndex = 3;
+            this.flItemListPanel.AutoScroll = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.flItemListPanel, 2);
+            this.flItemListPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flItemListPanel.Location = new System.Drawing.Point(218, 85);
+            this.flItemListPanel.Name = "flItemListPanel";
+            this.flItemListPanel.Size = new System.Drawing.Size(1045, 870);
+            this.flItemListPanel.TabIndex = 20;
             // 
-            // searchPanel
+            // btnPreviousPage
             // 
-            this.searchPanel.Controls.Add(this.btnSearch);
-            this.searchPanel.Location = new System.Drawing.Point(16, 13);
-            this.searchPanel.Name = "searchPanel";
-            this.searchPanel.Size = new System.Drawing.Size(1398, 87);
-            this.searchPanel.TabIndex = 13;
+            this.btnPreviousPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPreviousPage.BackColor = System.Drawing.Color.Peru;
+            this.btnPreviousPage.ForeColor = System.Drawing.Color.Black;
+            this.btnPreviousPage.Location = new System.Drawing.Point(1269, 905);
+            this.btnPreviousPage.Name = "btnPreviousPage";
+            this.btnPreviousPage.Size = new System.Drawing.Size(50, 50);
+            this.btnPreviousPage.TabIndex = 22;
+            this.btnPreviousPage.Text = "<-";
+            this.btnPreviousPage.UseVisualStyleBackColor = false;
+            this.btnPreviousPage.Click += new System.EventHandler(this.btnPreviousPage_Click);
+            // 
+            // btnNextPage
+            // 
+            this.btnNextPage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNextPage.BackColor = System.Drawing.Color.Peru;
+            this.btnNextPage.ForeColor = System.Drawing.Color.Black;
+            this.btnNextPage.Location = new System.Drawing.Point(1373, 905);
+            this.btnNextPage.Name = "btnNextPage";
+            this.btnNextPage.Size = new System.Drawing.Size(50, 50);
+            this.btnNextPage.TabIndex = 21;
+            this.btnNextPage.Text = "->";
+            this.btnNextPage.UseVisualStyleBackColor = false;
+            this.btnNextPage.Click += new System.EventHandler(this.btnNextPage_Click);
+            // 
+            // lblPageNumber
+            // 
+            this.lblPageNumber.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblPageNumber.AutoSize = true;
+            this.lblPageNumber.ForeColor = System.Drawing.Color.Black;
+            this.lblPageNumber.Location = new System.Drawing.Point(1325, 938);
+            this.lblPageNumber.Name = "lblPageNumber";
+            this.lblPageNumber.Size = new System.Drawing.Size(41, 20);
+            this.lblPageNumber.TabIndex = 23;
+            this.lblPageNumber.Text = "Page";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1426, 895);
-            this.Controls.Add(this.txtbSearchBar);
-            this.Controls.Add(this.btnPreviousPage);
-            this.Controls.Add(this.btnNextPage);
-            this.Controls.Add(this.flItemListPanel);
-            this.Controls.Add(this.btnHome);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.searchPanel);
+            this.ClientSize = new System.Drawing.Size(1426, 958);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.ForeColor = System.Drawing.Color.White;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Korkiet_aplikacja";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.searchPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btnHome;
-        private System.Windows.Forms.FlowLayoutPanel flItemListPanel;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtbSearchBar;
+        private System.Windows.Forms.Button btnHome;
+        private Ceneo.GUI.SpinningCircles spinningCircles1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.FlowLayoutPanel flItemListPanel;
         private System.Windows.Forms.Button btnNextPage;
         private System.Windows.Forms.Button btnPreviousPage;
-        private System.Windows.Forms.Button btnSearch;
-        private System.Windows.Forms.TextBox txtbSearchBar;
-        private System.Windows.Forms.Panel searchPanel;
+        private System.Windows.Forms.Label lblPageNumber;
     }
 }
 
